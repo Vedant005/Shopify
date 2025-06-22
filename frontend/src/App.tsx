41b8ac0df32a21f24fc819fa65cd83837cc05b6a
Vedant005 <vedantkanekar909@gmail.com>
2025-06-22 23:03:20 +0530

    chore : frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddItem from "./pages/AddItem";
+import ViewItems from "./pages/ViewItem";
+
+function App() {
+  return (
+    <>
+      <ViewItems />
+      <AddItem />
+    </>
+  );
+}
+
+export default App;
