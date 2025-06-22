3ade5159ac3ef81956c3fbfd03c4464cbf0b0ed7
Vedant005 <vedantkanekar909@gmail.com>
2025-06-23 01:48:55 +0530

    feat: refactor App component to implement routing with Header and separate pages for AddItem and ViewItems

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import AddItem from "./pages/AddItem";
 import ViewItems from "./pages/ViewItem";
 
 function App() {
   return (
-    <>
-      <ViewItems />
-      <AddItem />
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<AddItem />} />
+        <Route path="/view" element={<ViewItems />} />
+      </Routes>
+    </Router>
   );
 }
 
